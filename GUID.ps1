9bc552f0303b7f9ddfedd828a18d358efea5bf05
realscom <anthmilic@gmail.com>
2023-09-13 10:18:37 +1000

    yep.

---
+++ GUID.ps1
@@ -0,0 +1,2 @@
+# This will generate a nice guid for you. Useful in override MPs.
+[guid]::NewGuid().ToString("N")
---
+++ UnsealMP.ps1
@@ -0,0 +1,5 @@
+param($mpFilePath,$outputDirectory)
+$assembly = [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.EnterpriseManagement.OperationsManager")
+$mp = new-object Microsoft.EnterpriseManagement.Configuration.ManagementPack($mpFilePath)
+$mpWriter = new-object Microsoft.EnterpriseManagement.Configuration.IO.ManagementPackXmlWriter($outputDirectory)
+$mpWriter.WriteManagementPack($mp)

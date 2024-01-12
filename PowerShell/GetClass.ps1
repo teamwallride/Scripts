cf1d2b0ed79da0555b96ee4c3ebf4c87838de09d
realscom <anthmilic@gmail.com>
2024-01-12 16:36:03 +1100

    Update output so it looks better.

--- PowerShell/GetClass.ps1
+++ PowerShell/GetClass.ps1
@@ -1,16 +1,14 @@
+# This only shows Name, DisplayName and Id to fit on screen.
 param($Type, $Class)
 Write-Host
-Write-Host -ForegroundColor yellow "Searching for class:" $Class "(sorted by 1st column)"
-if ($Type -eq "n")
-{
-Get-SCOMClass -Name "*$Class*" | sort Name | ft Name, DisplayName, ManagementPackName, Abstract, Hosted, Id -au
+Write-Host -ForegroundColor yellow "Getting classes..."
+if ($Type -eq "n") {
+    Get-SCOMClass -Name "*$Class*" | sort Name | ft Name, DisplayName, Id -a
 }
-elseif ($Type -eq "d")
-{
-Get-SCOMClass -DisplayName "*$Class*" | sort DisplayName | ft DisplayName, Name, ManagementPackName, Abstract, Hosted, Id -au
+elseif ($Type -eq "d") {
+    Get-SCOMClass -DisplayName "*$Class*" | sort DisplayName | ft DisplayName, Name, Id -au
 }
-elseif ($Type -eq "i")
-{
-Get-SCOMClass -id "$Class" | sort DisplayName | ft DisplayName, Name, ManagementPackName, Abstract, Hosted, Id -au
+elseif ($Type -eq "i") {
+    Get-SCOMClass -id "$Class" | sort DisplayName | ft DisplayName, Name, Id -au
 }
 
--- PowerShell/GetClassInstance.ps1
+++ PowerShell/GetClassInstance.ps1
@@ -1,15 +1,13 @@
+# This only shows DisplayName, Path and Id to fit on screen.
 param($Type, $Class)
 Write-Host
-Write-Host -ForegroundColor yellow "Looking for instances:" $Class
-Write-Host
-if ($Type -eq "n")
-{
-$a = get-scomclass -name "$Class" | get-scomclassinstance | sort displayname
+Write-Host -ForegroundColor yellow "Getting class instances..."
+if ($Type -eq "n") {
+    get-scomclass -name "$Class" | get-scomclassinstance | sort Name | ft DisplayName, Path, Id -au
 }
-elseif ($Type -eq "d")
-{
-$a = get-scomclass -displayname "$Class" | get-scomclassinstance | sort displayname
+elseif ($Type -eq "d") {
+    get-scomclass -displayname "$Class" | get-scomclassinstance | sort Name | ft DisplayName, Path, Id -au
 }
-foreach ($i in $a) {write-host $i.displayname","$i.path","$i.id}
+foreach ($i in $a) { write-host $i.displayname"^"$i.path"^"$i.id }
 write-host
 write-host "Count: " $a.count

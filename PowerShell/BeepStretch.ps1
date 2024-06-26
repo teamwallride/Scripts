2c4b97b42da3b38369b422c3eb4b678a69bf0eab
realscom <anthmilic@gmail.com>
2024-06-26 14:04:35 +1000

    Add audible count.

--- PowerShell/BeepStretch.ps1
+++ PowerShell/BeepStretch.ps1
@@ -1,4 +1,17 @@
+<# This makes the computer beep.
 1..10 | ForEach-Object {
     [console]::beep(3000, 500) # First number is pitch, second number is duration.
     Start-Sleep 30
+}
+#>
+
+# This is cooler, it counts.
+Add-Type -AssemblyName System.Speech
+$synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+1..10 | ForEach-Object {
+    $synth.Speak($_)
+    Start-Sleep 3
+    if ($_ -eq 10) {
+        $synth.Speak("Stretching complete")
+    }
 }

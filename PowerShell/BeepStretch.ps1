0984dd7df367b09a631381ee7e199cd1618d873b
realscom <anthmilic@gmail.com>
2024-06-26 13:49:43 +1000

    Formatted.

--- PowerShell/BeepStretch.ps1
+++ PowerShell/BeepStretch.ps1
@@ -1,4 +1,4 @@
 1..10 | ForEach-Object {
-    [console]::beep(3000,500) # first number is pitch, second number is duration.
+    [console]::beep(3000, 500) # First number is pitch, second number is duration.
     Start-Sleep 30
-    }
+}

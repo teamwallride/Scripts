3bbdf28b9b7fa07f8319bd659fb98255542afba7
realscom <anthmilic@gmail.com>
2024-06-26 14:08:09 +1000

    Change sleep timer.

--- PowerShell/BeepStretch.ps1
+++ PowerShell/BeepStretch.ps1
@@ -10,7 +10,7 @@ Add-Type -AssemblyName System.Speech
 $synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
 1..10 | ForEach-Object {
     $synth.Speak($_)
-    Start-Sleep 3
+    Start-Sleep 30
     if ($_ -eq 10) {
         $synth.Speak("Stretching complete")
     }
